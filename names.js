05b1d4391b73e0d405e00ecc193539465930509c
Rio Cantre <smittenaf301icu@gmail.com>
2022-10-24 11:14:56 +0200

    Added Info

--- names.js
+++ names.js
@@ -1,140 +1,144 @@
 const names = [
-  {
-  name: "Rajkumar",
-  fullName: "Rajkumar",
-  githubUrl: "https://github.com/Rajkumar-justcoder",
-}, 
-{
-  name: "AnshSinghSonkhia",
-  fullName: "Ansh Singh Sonkhia",
-  githubUrl: "https://github.com/AnshSinghSonkhia",
-}, 
-{
-  name: "rhvsingh",
-  fullName: "Raja Harsh Vardhan Singh",
-  githubUrl: "https://github.com/rhvsingh",
-},
-{
-  name: "MilindPalaria",
-  fullName: "Milind-Palaria",
-  githubUrl: "https://github.com/Milind-Palaria",
-}, 
-{
-  name: "Ritik",
-  fullName: "Ritik Jha",
-  githubUrl: "https://github.com/ritikjha5700",
-}, 
-{
-  name: "Ayobami",
-  fullName: "Ayobami Seun",
-  githubUrl: "https://github.com/ayobamiseun",
-}, 
-{
-  name: "Devraj",
-  fullName: "Devraj Chatribin",
-  githubUrl: "https://github.com/DevrajDC",
-}, 
-{
-  name: "Swaroop",
-  fullName: "Swaroop Rajwal",
-  githubUrl: "https://github.com/swaroopRajwal",
-},
-{
-    name: "Gambhir",
-    fullName: "Gambhir Sharma",
-    githubUrl: "https://github.com/Gambhirsharma",
-  },
-  {
+    {
+        name: 'Rajkumar',
+        fullName: 'Rajkumar',
+        githubUrl: 'https://github.com/Rajkumar-justcoder',
+    },
+    {
+        name: 'AnshSinghSonkhia',
+        fullName: 'Ansh Singh Sonkhia',
+        githubUrl: 'https://github.com/AnshSinghSonkhia',
+    },
+    {
+        name: 'rhvsingh',
+        fullName: 'Raja Harsh Vardhan Singh',
+        githubUrl: 'https://github.com/rhvsingh',
+    },
+    {
+        name: 'MilindPalaria',
+        fullName: 'Milind-Palaria',
+        githubUrl: 'https://github.com/Milind-Palaria',
+    },
+    {
+        name: 'Ritik',
+        fullName: 'Ritik Jha',
+        githubUrl: 'https://github.com/ritikjha5700',
+    },
+    {
+        name: 'Ayobami',
+        fullName: 'Ayobami Seun',
+        githubUrl: 'https://github.com/ayobamiseun',
+    },
+    {
+        name: 'Devraj',
+        fullName: 'Devraj Chatribin',
+        githubUrl: 'https://github.com/DevrajDC',
+    },
+    {
+        name: 'Swaroop',
+        fullName: 'Swaroop Rajwal',
+        githubUrl: 'https://github.com/swaroopRajwal',
+    },
+    {
+        name: 'Gambhir',
+        fullName: 'Gambhir Sharma',
+        githubUrl: 'https://github.com/Gambhirsharma',
+    },
+    {
+        name: 'Angel',
+        fullName: 'Angel Bansal',
+        githubUrl: 'https://github.com/abn15',
+    },
+    {
+        name: 'Uday',
+        fullName: 'Uday Mehtani',
+        githubUrl: 'https://github.com/uday03meh',
+    },
+    {
+        name: 'Nitesh',
+        fullName: 'Nitesh Thapliyal',
+        githubUrl: 'https://github.com/Nitesh-thapliyal',
+    },
+    {
+        name: 'Deepanshu',
+        fullName: 'Deepanshu Manocha',
+        githubUrl: 'https://github.com/dmanocha464',
+    },
+    {
+        name: 'Piyush',
+        fullName: 'Piyush Chandel',
+        githubUrl: 'https://github.com/Piyush-Chandel7',
+    },
+    {
+        name: 'Binil',
+        fullName: 'Binil Tom Jose',
+        githubUrl: 'https://github.com/BinilTomJose1278',
+    },
+    {
+        name: 'Taj',
+        fullName: 'Shaik Taj Baba',
+        githubUrl: 'https://github.com/tajbaba999',
+    },
+    {
+        name: 'Shivanshi',
+        fullName: 'Shivanshi Saxena',
+        githubUrl: 'https://github.com/shivanshi-s',
+    },
+    {
+        name: 'Dia',
+        fullName: 'DiaBolical0212',
+        githubUrl: 'https://github.com/DiaBolical0212/',
+    },
+    {
+        name: 'Stuti',
+        fullName: 'Stuti Upreti',
+        githubUrl: 'https://github.com/thestuti',
+    },
+    {
+        name: 'Gaurav',
+        fullName: 'Gaurav Chatribin',
+        githubUrl: 'https://github.com/gaurav-chatribin',
+    },
+    {
+        name: 'Sabin',
+        fullName: 'Sabin Thapa',
+        githubUrl: 'https://github.com/sabin-thapa',
+    },
+    {
+        name: 'Francis',
+        fullName: 'Francis Okonkwo',
+        githubUrl: 'https://github.com/Bluebberries',
+    },
+    {
+        name: 'Ganesh Karthik',
+        fullName: 'Ganesh Karthik Ramarathinam',
+        githubUrl: 'https://github.com/Ganeshkarthik1031',
+    },
+    {
+        name: 'shiv',
+        fullName: 'Shivam Singh',
+        githubUrl: 'https://github.com/shiv-03',
+    },
+    {
+        name: 'Rahul',
+        fullName: 'Rahul Raj Sarma',
+        githubUrl: 'https://github.com/ragingrahul',
+    },
+    {
+        name: 'Rishabh Pratap Singh',
+        fullName: 'Rishabh',
+        githubUrl: 'https://github.com/iam-Rps',
+    },
+    {
+        name: 'pranath',
+        fullName: 'Pranath Naik',
+        githubUrl: 'https://github.com/pranathnaik',
+    },
+    {
+        name: 'Rio',
+        fullName: 'Rio Cantre',
+        githubUrl: 'https://github.com/RioCantre',
+    },
+];
 
-  name: "Angel",
-  fullName: "Angel Bansal",
-  githubUrl: "https://github.com/abn15",
-}, {
-  name: "Uday",
-  fullName: "Uday Mehtani",
-  githubUrl: "https://github.com/uday03meh",
-},
-{
-  name: "Nitesh",
-  fullName: "Nitesh Thapliyal",
-  githubUrl: "https://github.com/Nitesh-thapliyal",
-},
-{
-  name: "Deepanshu",
-  fullName: "Deepanshu Manocha",
-  githubUrl: "https://github.com/dmanocha464",
-},
-{
-  name: "Piyush",
-  fullName: "Piyush Chandel",
-  githubUrl: "https://github.com/Piyush-Chandel7",
-},
-{
-  name: "Binil",
-  fullName:"Binil Tom Jose",
-  githubUrl:"https://github.com/BinilTomJose1278"
-},
-{
-  name: "Taj",
-  fullName:"Shaik Taj Baba",
-  githubUrl:"https://github.com/tajbaba999"
-},
-{
-  name: "Shivanshi",
-  fullName:"Shivanshi Saxena",
-  githubUrl:"https://github.com/shivanshi-s"
-},
-{
-  name: "Dia",
-  fullName:"DiaBolical0212",
-githubUrl:"https://github.com/DiaBolical0212/"
-},
-{
-  name: "Stuti",
-  fullName:"Stuti Upreti",
-  githubUrl:"https://github.com/thestuti"
-},
-{
-  name: "Gaurav",
-  fullName:"Gaurav Chatribin",
-  githubUrl:"https://github.com/gaurav-chatribin"
-},
-  {
-  name: "Sabin",
-  fullName:"Sabin Thapa",
-  githubUrl:"https://github.com/sabin-thapa"
-},
-{
-    name: 'Francis',
-    fullName: 'Francis Okonkwo',
-    githubUrl: 'https://github.com/Bluebberries'
-},
-{
-    name: 'Ganesh Karthik',
-    fullName: 'Ganesh Karthik Ramarathinam',
-    githubUrl: 'https://github.com/Ganeshkarthik1031'
-},
-  {
-  name: "shiv",
-  fullName: "Shivam Singh",
-  githubUrl: "https://github.com/shiv-03",
-},
-{
-  name: 'Rahul',
-  fullName: 'Rahul Raj Sarma',
-  githubUrl: 'https://github.com/ragingrahul'
-},
-  {
-  name: "Rishabh Pratap Singh",
-  fullName: "Rishabh",
-  githubUrl: "https://github.com/iam-Rps",
-},
-{
-  name: "pranath",
-  fullName: "Pranath Naik",
-  githubUrl: "https://github.com/pranathnaik",
-}
-
-]
-
-export default names
+export default names;
